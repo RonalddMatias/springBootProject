6774da2327f9174fbf92a70b9dfa3416549bb1ad
RonalddMatias <ronaldd.feliph.matias.costa@ccc.ufcg.edu.br>
2024-08-03 10:44:27 -0300

    Service Layer and Component Registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -2,8 +2,9 @@ package com.educandoweb.course.repositories;
 
 import com.educandoweb.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // O que vai no <> é a entidade e o tipo dela
+@Repository // essa annotation nao era necessario pois jpaRepository ja é componente do Spring
 public interface UserRepository extends JpaRepository<User, Long> {
-
 }
